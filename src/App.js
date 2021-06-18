27ba8573aba206c9a7f94711d4f1df18a5601bcd
lamthieu <quanglam.thieu@yahoo.com>
2021-06-18 21:20:05 +0700

    update structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,89 @@
-import React from "react"
+import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from 'react-router-dom';
+import Layout from './layouts/default';
 
-function App() {
-    return (<div>
-        <h2>Welcome to React App</h2>
-        <h3>Date : {new Date().toDateString()}</h3>
-    </div>)
+export default function App() {
+  return (
+    <Router>
+      <Layout>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/topics'>Topics</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/topics'>
+            <Topics />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </Layout>
+    </Router>
+  );
 }
 
-export default App
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topics() {
+  let match = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props v. State
+          </Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an 'index' page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react"
-import ReactDom from "react-dom"
-import App from "./App"
-import "./App.scss"
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+import './App.scss';
 
-ReactDom.render(<App />, document.getElementById('app'))
+ReactDom.render(<App />, document.getElementById('app'));
